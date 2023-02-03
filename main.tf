64723f730642bf60c9ff0ceee564584e362ef3b4
thunikisateesh <thunikisateesh@gmail.com>
2023-02-03 19:44:28 +0530

    new resource add

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
-resource "resource_group" "rg" {
+resource "azurerm_resource_group" "rg" {
     name = "myrg"
     location = "westus"
   
+}
+
+resource "azurerm_storage_account" "name" {
+  name = "mystroageaddoyn"
+  location = azurerm_resourouce_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
